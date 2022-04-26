6d0f0f22d49fd5613c95edf7eed92af9d04d4fe6
Rodrigo Cuadra <69258390+rodrigoCuadraCaro@users.noreply.github.com>
2022-04-25 22:01:50 -0400

    added modificar org and various fixes

--- app/src/main/java/com/example/mieventoapp/ModificarPerfilOrg.java
+++ app/src/main/java/com/example/mieventoapp/ModificarPerfilOrg.java
@@ -2,13 +2,36 @@ package com.example.mieventoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ModificarPerfilOrg extends AppCompatActivity {
+
+    private Button bttnModificarPerfilOrg, bttnVolverOrg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modificar_perfil_org);
+
+        bttnModificarPerfilOrg = (Button) findViewById(R.id.bttnModificarPerfilOrg);
+        bttnVolverOrg = (Button) findViewById(R.id.bttnVolverOrg);
+
+        Buttons();
+
     }
+
+    private void Buttons(){
+        bttnVolverOrg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(ModificarPerfilOrg.this, MenuOrganizador.class);
+                startActivity(i);
+                finish();
+            }
+        });
+    }
+
 }
