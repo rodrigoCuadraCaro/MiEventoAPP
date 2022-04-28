7308b3176ee29dc287a9d0c81d2d331a86c7fff0
Rodrigo Cuadra <69258390+rodrigoCuadraCaro@users.noreply.github.com>
2022-04-28 03:15:23 -0400

    [+]implemented BD

--- app/src/main/java/com/example/mieventoapp/ventanas/MainActivity.java
+++ app/src/main/java/com/example/mieventoapp/ventanas/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mieventoapp.ventanas;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,13 +8,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
 
 import com.example.mieventoapp.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText loginEmail, loginPassword;
     private Button bttnLogin, bttnCrearCuenta, bttnCrearCuentaOrg;
+    private AsyncHttpClient client;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
         bttnLogin = (Button) findViewById(R.id.bttnLogin);
         bttnCrearCuenta = (Button) findViewById(R.id.bttnCrearCuenta);
         bttnCrearCuentaOrg = (Button) findViewById(R.id.bttnCrearCuentaOrg);
+        client = new AsyncHttpClient();
 
         Buttons();
     }
@@ -33,7 +44,34 @@ public class MainActivity extends AppCompatActivity {
         bttnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
+
                 String email = loginEmail.getText().toString();
+                String password = loginPassword.getText().toString();
+
+                String url = "https://mieventoappinacap.000webhostapp.com/next/validateUser.php?nombre="+email+"&pass="+password;
+                System.out.println(url);
+
+                client.post(url, new AsyncHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                        if (statusCode == 200){
+                            Intent i = new Intent(MainActivity.this, MenuAdmin.class);
+                            startActivity(i);
+                            finish();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                        AlertDialog.Builder msg = new AlertDialog.Builder(MainActivity.this);
+                        msg.setTitle("Cuenta deshabilitada");
+                        msg.setMessage("Su cuenta se encuentre bloqueda o no se encuentra disponible" +
+                                " en este momento. Comuniquese con un administrador para solicitar ayuda");
+                        msg.show();
+                    }
+                });
+
+                /*
                 if(email.equals("Admin")){
                     Intent i = new Intent(MainActivity.this, MenuAdmin.class);
                     startActivity(i);
@@ -47,6 +85,8 @@ public class MainActivity extends AppCompatActivity {
                     startActivity(i);
                     finish();
                 }
+
+                 */
             }
         });
 
