3ffe07b8a809b9d40c865a9f55ce0978598dcb2b
Rodrigo Cuadra <69258390+rodrigoCuadraCaro@users.noreply.github.com>
2022-04-28 23:04:26 -0400

    added login to BD

---
+++ app/src/main/java/com/example/mieventoapp/Clases/Usuarios.java
@@ -0,0 +1,154 @@
+package com.example.mieventoapp.Clases;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Usuarios implements Parcelable {
+
+    private int id;
+    private String correo;
+    private String password;
+    private String name;
+    private int estado;
+    private String estadoStr;
+    private int tipo;
+    private String tipoStr;
+
+    public Usuarios() {
+
+    }
+
+    public Usuarios(int id, String correo, String password, String name, int estado, int tipo) {
+        this.id = id;
+        this.correo = correo;
+        this.password = password;
+        this.name = name;
+        this.estado = estado;
+        this.tipo = tipo;
+    }
+
+    public Usuarios(int id, String correo, String password, String name, String estadoStr, String tipoStr) {
+        this.id = id;
+        this.correo = correo;
+        this.password = password;
+        this.name = name;
+        this.estadoStr = estadoStr;
+        this.tipoStr = tipoStr;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+
+    public String getEstadoStr() {
+        return estadoStr;
+    }
+
+    public void setEstadoStr(String estadoStr) {
+        this.estadoStr = estadoStr;
+    }
+
+    public int getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getTipoStr() {
+        return tipoStr;
+    }
+
+    public void setTipoStr(String tipoStr) {
+        this.tipoStr = tipoStr;
+    }
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(this.id);
+        dest.writeString(this.correo);
+        dest.writeString(this.password);
+        dest.writeString(this.name);
+        dest.writeInt(this.estado);
+        dest.writeString(this.estadoStr);
+        dest.writeInt(this.tipo);
+        dest.writeString(this.tipoStr);
+    }
+
+    public void readFromParcel(Parcel source) {
+        this.id = source.readInt();
+        this.correo = source.readString();
+        this.password = source.readString();
+        this.name = source.readString();
+        this.estado = source.readInt();
+        this.estadoStr = source.readString();
+        this.tipo = source.readInt();
+        this.tipoStr = source.readString();
+    }
+
+    protected Usuarios(Parcel in) {
+        this.id = in.readInt();
+        this.correo = in.readString();
+        this.password = in.readString();
+        this.name = in.readString();
+        this.estado = in.readInt();
+        this.estadoStr = in.readString();
+        this.tipo = in.readInt();
+        this.tipoStr = in.readString();
+    }
+
+    public static final Creator<Usuarios> CREATOR = new Creator<Usuarios>() {
+        @Override
+        public Usuarios createFromParcel(Parcel source) {
+            return new Usuarios(source);
+        }
+
+        @Override
+        public Usuarios[] newArray(int size) {
+            return new Usuarios[size];
+        }
+    };
+}
--- app/src/main/java/com/example/mieventoapp/ventanas/MainActivity.java
+++ app/src/main/java/com/example/mieventoapp/ventanas/MainActivity.java
@@ -8,11 +8,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.example.mieventoapp.Clases.Usuarios;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 
 import com.example.mieventoapp.R;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -55,18 +58,66 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
                         if (statusCode == 200){
+                            System.out.println("here");
+                            /*
                             Intent i = new Intent(MainActivity.this, MenuAdmin.class);
                             startActivity(i);
                             finish();
+                             */
+                            try {
+                                JSONObject json = new JSONObject(new String(responseBody));
+                                Usuarios u = new Usuarios();
+                                u.setId(json.getInt("id_usu"));
+                                u.setCorreo(json.getString("correo"));
+                                u.setPassword(json.getString("pass"));
+                                u.setName(json.getString("nombre"));
+                                u.setEstado(json.getInt("id_est"));
+                                u.setTipo(json.getInt("id_tip"));
+
+                                if (u.getEstado() == 2){
+                                    AlertDialog.Builder msg = new AlertDialog.Builder(MainActivity.this);
+                                    msg.setTitle("Cuenta deshabilitada");
+                                    msg.setMessage("Su cuenta se encuentra bloqueda o no se encuentra disponible" +
+                                            " en este momento. Comuniquese con un administrador para solicitar ayuda");
+                                    msg.show();
+                                } else {
+                                    if (u.getTipo() == 1){
+                                        Intent in = new Intent(MainActivity.this, MenuAdmin.class);
+                                        in.putExtra("user", u);
+                                        startActivity(in);
+                                        finish();
+                                    } else if (u.getTipo() == 2){
+                                        Intent in = new Intent(MainActivity.this, MenuAsistente.class);
+                                        in.putExtra("user", u);
+                                        startActivity(in);
+                                        finish();
+                                    } else if (u.getTipo() == 3){
+                                        Intent in = new Intent(MainActivity.this, MenuOrganizador.class);
+                                        in.putExtra("user", u);
+                                        startActivity(in);
+                                        finish();
+                                    } else {
+                                        AlertDialog.Builder msg = new AlertDialog.Builder(MainActivity.this);
+                                        msg.setTitle("Ha ocurrido un error");
+                                        msg.setMessage("Error inesperado, " +
+                                                "si el error persiste comuniquese con soporte");
+                                        msg.show();
+                                    }
+                                    
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
                         }
                     }
 
                     @Override
                     public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
                         AlertDialog.Builder msg = new AlertDialog.Builder(MainActivity.this);
-                        msg.setTitle("Cuenta deshabilitada");
-                        msg.setMessage("Su cuenta se encuentre bloqueda o no se encuentra disponible" +
-                                " en este momento. Comuniquese con un administrador para solicitar ayuda");
+                        msg.setTitle("Error");
+                        msg.setMessage("Error en sistema, porfavor intente nuevamente, " +
+                                "si el error persiste comuniquese con soporte");
                         msg.show();
                     }
                 });
