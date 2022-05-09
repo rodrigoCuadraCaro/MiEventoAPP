4db543c0d6e2f5e23eeb2c001e6d51e405aeec1e
Rodrigo Cuadra <69258390+rodrigoCuadraCaro@users.noreply.github.com>
2022-05-08 22:24:19 -0400

    [+]fixed bugs

---
+++ app/src/main/java/com/example/mieventoapp/ventanas/AgregarEvento.java
@@ -0,0 +1,15 @@
+package com.example.mieventoapp.ventanas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.mieventoapp.R;
+
+public class AgregarEvento extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_evento);
+    }
+}
